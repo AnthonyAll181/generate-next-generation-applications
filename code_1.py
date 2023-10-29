3d7a1bd05094174466e45b6bf9bd00cafa1e4658
Anthony Allison <139900552+AnthonyAll181@users.noreply.github.com>
2023-10-29 17:40:03 +0100

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,68 +1,25 @@
-
-import random
-if __name__ == "__main__":
-        print(f"Random Number: {item}")
 def generate_random_data():
 
     main()
-    for item in data:
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+
     return data
-def main():
-    data = [random.randint(1, 100) for _ in range(10)]
-    data = generate_random_data()
 
-    data = [random.randint(1, 100) for _ in range(10)]
-
-    for item in data:
-
-    main()
-
-def generate_random_data():
-def main():
-        print(f"Random Number: {item}")
 if __name__ == "__main__":
-
-    data = generate_random_data()
 import random
-    return data    main()
-    return data
-
-if __name__ == "__main__":
-        print(f"Random Number: {item}")
-
-    data = generate_random_data()
     for item in data:
 
-def generate_random_data():
-
-def main():
-    data = [random.randint(1, 100) for _ in range(10)]
-import random    });
-
-        data.push(Math.floor(Math.random() * 100) + 1);
-function main() {
-}
-    }
-    return data;
-main();
-function generateRandomData() {
-    data.forEach(item => {
-    const data = generateRandomData();
-
-    for (let i = 0; i < 10; i++) {
-    const data = [];
-
-}
-        console.log("Random Number: " + item);        for (int i = 0; i < 10; i++) {
-    public static void main(String[] args) {
-
-        for (int item : data) {
-            System.out.println("Random Number: " + item);
-        int[] data = new int[10];
-        }
-}
+        print(f"Random Number: {item}")
+    data = generate_random_data()            System.out.println("Random Number: " + item);
 public class RandomDataGenerator {
-
-        }
             data[i] = (int) (Math.random() * 100) + 1;
+}
+        }
+        int[] data = new int[10];
     }
+        for (int i = 0; i < 10; i++) {
+        }
+
+    public static void main(String[] args) {
+        for (int item : data) {
