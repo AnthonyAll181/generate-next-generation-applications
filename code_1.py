6db8d729bd7bca077a759c3bedae31f8ac0b9729
Anthony Allison <139900552+AnthonyAll181@users.noreply.github.com>
2023-11-04 17:47:00 +0100

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,46 +1,52 @@
+    }
+        int[] data = new int[10];
+}
+
+    public static void main(String[] args) {
+public class RandomDataGenerator {
+        for (int item : data) {
+            data[i] = (int) (Math.random() * 100) + 1;
+        }
+            System.out.println("Random Number: " + item);
+        for (int i = 0; i < 10; i++) {
+
+        }
+    main()
+import random
     data = [random.randint(1, 100) for _ in range(10)]
 
 if __name__ == "__main__":
-def generate_random_data():
-        print(f"Random Number: {item}")
-import random
+    return data
     for item in data:
 
+    data = generate_random_data()
+        print(f"Random Number: {item}")
 def main():
+def generate_random_data():
+    main()
 
 
     data = generate_random_data()
+import random
+    for item in data:
     return data
-    main()function generateRandomData() {
-        console.log("Random Number: " + item);
 
-    const data = [];
-function main() {
+def generate_random_data():
+if __name__ == "__main__":
+        print(f"Random Number: {item}")
 
-}
-    return data;
-    });
-    const data = generateRandomData();
-    for (let i = 0; i < 10; i++) {
-    }
-        data.push(Math.floor(Math.random() * 100) + 1);
-main();
-}
+    data = [random.randint(1, 100) for _ in range(10)]
+def main():def main():
+    for item in data:
 
-    data.forEach(item => {main();
-        data.push(Math.floor(Math.random() * 100) + 1);
+    data = generate_random_data()
+    return data
+if __name__ == "__main__":
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
 
-}
-        console.log("Random Number: " + item);
-    return data;
-}
 
-    for (let i = 0; i < 10; i++) {
-    });
-    const data = generateRandomData();
-    const data = [];
-    data.forEach(item => {
-function generateRandomData() {
-    }
+def generate_random_data():
 
-function main() {
+    main()
